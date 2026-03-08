fad277b2ebf754f340de66fdaec5048161e4c337
dhruv4023 <dhruv20345@gmail.com>
2026-03-08 21:58:06 +0530

    langgraph

--- workflows/note_workflow.py
+++ workflows/note_workflow.py
@@ -3,8 +3,58 @@ from services.github_service import fetch_github_content
 from services.gemini_service import generate_ai_output
 from services.spring_service import update_note
 
-inngest_client = Inngest(app_id="code-note-ai")
 
+from langgraph.graph import StateGraph
+from typing import TypedDict, List
+
+class NoteState(TypedDict):
+    notes: List[dict]
+
+async def fetch_code(state: NoteState):
+
+    for note in state["notes"]:
+        note["github_content"] = await fetch_github_content(
+            note["permanentLink"]
+        )
+
+    return state
+
+
+async def generate_ai(state: NoteState):
+    for note in state["notes"]:
+        note["ai_result"] = await generate_ai_output(note["title"], note["note"], note["github_content"])
+
+        # note["ai_result"] = {
+        #     "summary": "This is a summary",
+        #     "explanation": "This is an explanation",
+        #     "improvements": "This is improvements",
+        #     "tags": ["tag1", "tag2"]
+        # }
+
+    return state
+
+async def save_notes(state: NoteState):
+
+    for note in state["notes"]:
+        await update_note(note["id"], note["ai_result"])
+
+    return state
+
+
+graph = StateGraph(NoteState)
+
+graph.add_node("fetch", fetch_code)
+graph.add_node("ai", generate_ai)
+graph.add_node("save", save_notes)
+
+graph.set_entry_point("fetch")
+
+graph.add_edge("fetch", "ai")
+graph.add_edge("ai", "save")
+
+agent = graph.compile()
+
+inngest_client = Inngest(app_id="code-note-ai")
 @inngest_client.create_function(
     fn_id="ai-note-enrichment",
     trigger=TriggerEvent(event="note/created"),
@@ -15,20 +65,10 @@ async def ai_enrichment(ctx):
 
     data = ctx.event.data
 
-    note_id = data["noteId"]
-    title = data["title"]
-    note = data["note"]
-    permalink = data["permanentLink"]
+    state = {
+        "notes": data["items"]
+    }
 
-    github_content = await fetch_github_content(permalink)
-    ai_result = await generate_ai_output(title, note, github_content)
-    # ai_result = {
-    #     "summary": "AI Summary",
-    #     "explanation": "AI Explanation",
-    #     "improvements": "AI Improvements",
-    #     "tags": ["python", "java"]
-    # }
+    await agent.ainvoke(state)
 
-    await update_note(note_id, ai_result)
-
-    return {"status": "completed"}
+    return {"status": "processed"}
